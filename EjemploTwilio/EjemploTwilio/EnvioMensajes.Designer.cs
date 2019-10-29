af9f089a6d520097342f0f0d29db5f33b04cb86e
Arturo Aymerich Panayotte <aaymerichp@ucenfotec.ac.cr>
2019-10-29 09:25:13 -0600

    Add files via upload

---
+++ EjemploTwilio/EjemploTwilio/EnvioMensajes.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace EjemploTwilio
+{
+    partial class EnvioMensajes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.senderTB = new System.Windows.Forms.TextBox();
+            this.WsenderTB = new System.Windows.Forms.TextBox();
+            this.toTB = new System.Windows.Forms.TextBox();
+            this.WtoTB = new System.Windows.Forms.TextBox();
+            this.mensajeTB = new System.Windows.Forms.RichTextBox();
+            this.WmensajeTB = new System.Windows.Forms.RichTextBox();
+            this.smssendBT = new System.Windows.Forms.Button();
+            this.WsmsBT = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.detalleTB = new System.Windows.Forms.RichTextBox();
+            this.WdetalleTB = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // senderTB
+            // 
+            this.senderTB.Enabled = false;
+            this.senderTB.Location = new System.Drawing.Point(79, 92);
+            this.senderTB.Name = "senderTB";
+            this.senderTB.Size = new System.Drawing.Size(100, 20);
+            this.senderTB.TabIndex = 0;
+            this.senderTB.Text = "+15732270574";
+            // 
+            // WsenderTB
+            // 
+            this.WsenderTB.Enabled = false;
+            this.WsenderTB.Location = new System.Drawing.Point(436, 92);
+            this.WsenderTB.Name = "WsenderTB";
+            this.WsenderTB.Size = new System.Drawing.Size(136, 20);
+            this.WsenderTB.TabIndex = 1;
+            this.WsenderTB.Text = "+14155238886";
+            // 
+            // toTB
+            // 
+            this.toTB.Enabled = false;
+            this.toTB.Location = new System.Drawing.Point(79, 145);
+            this.toTB.Name = "toTB";
+            this.toTB.Size = new System.Drawing.Size(100, 20);
+            this.toTB.TabIndex = 2;
+            this.toTB.Text = "87191177";
+            // 
+            // WtoTB
+            // 
+            this.WtoTB.Enabled = false;
+            this.WtoTB.Location = new System.Drawing.Point(436, 145);
+            this.WtoTB.Name = "WtoTB";
+            this.WtoTB.Size = new System.Drawing.Size(100, 20);
+            this.WtoTB.TabIndex = 3;
+            this.WtoTB.Text = "87191177";
+            // 
+            // mensajeTB
+            // 
+            this.mensajeTB.Location = new System.Drawing.Point(79, 189);
+            this.mensajeTB.Name = "mensajeTB";
+            this.mensajeTB.Size = new System.Drawing.Size(174, 58);
+            this.mensajeTB.TabIndex = 4;
+            this.mensajeTB.Text = "";
+            // 
+            // WmensajeTB
+            // 
+            this.WmensajeTB.Location = new System.Drawing.Point(436, 189);
+            this.WmensajeTB.Name = "WmensajeTB";
+            this.WmensajeTB.Size = new System.Drawing.Size(174, 58);
+            this.WmensajeTB.TabIndex = 5;
+            this.WmensajeTB.Text = "";
+            // 
+            // smssendBT
+            // 
+            this.smssendBT.Location = new System.Drawing.Point(79, 284);
+            this.smssendBT.Name = "smssendBT";
+            this.smssendBT.Size = new System.Drawing.Size(75, 23);
+            this.smssendBT.TabIndex = 6;
+            this.smssendBT.Text = "Enviar";
+            this.smssendBT.UseVisualStyleBackColor = true;
+            this.smssendBT.Click += new System.EventHandler(this.smssendBT_Click);
+            // 
+            // WsmsBT
+            // 
+            this.WsmsBT.Location = new System.Drawing.Point(436, 284);
+            this.WsmsBT.Name = "WsmsBT";
+            this.WsmsBT.Size = new System.Drawing.Size(75, 23);
+            this.WsmsBT.TabIndex = 7;
+            this.WsmsBT.Text = "Enviar";
+            this.WsmsBT.UseVisualStyleBackColor = true;
+            this.WsmsBT.Click += new System.EventHandler(this.WsmsBT_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "De : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(400, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "De : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Para : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(392, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Para : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(124, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "SMS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(471, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "WhatsApp";
+            // 
+            // detalleTB
+            // 
+            this.detalleTB.Enabled = false;
+            this.detalleTB.Location = new System.Drawing.Point(79, 313);
+            this.detalleTB.Name = "detalleTB";
+            this.detalleTB.Size = new System.Drawing.Size(236, 113);
+            this.detalleTB.TabIndex = 14;
+            this.detalleTB.Text = "";
+            // 
+            // WdetalleTB
+            // 
+            this.WdetalleTB.Enabled = false;
+            this.WdetalleTB.Location = new System.Drawing.Point(436, 313);
+            this.WdetalleTB.Name = "WdetalleTB";
+            this.WdetalleTB.Size = new System.Drawing.Size(252, 113);
+            this.WdetalleTB.TabIndex = 15;
+            this.WdetalleTB.Text = "";
+            // 
+            // EnvioMensajes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 455);
+            this.Controls.Add(this.WdetalleTB);
+            this.Controls.Add(this.detalleTB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WsmsBT);
+            this.Controls.Add(this.smssendBT);
+            this.Controls.Add(this.WmensajeTB);
+            this.Controls.Add(this.mensajeTB);
+            this.Controls.Add(this.WtoTB);
+            this.Controls.Add(this.toTB);
+            this.Controls.Add(this.WsenderTB);
+            this.Controls.Add(this.senderTB);
+            this.Name = "EnvioMensajes";
+            this.Text = "EnvioMensajes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox senderTB;
+        private System.Windows.Forms.TextBox WsenderTB;
+        private System.Windows.Forms.TextBox toTB;
+        private System.Windows.Forms.TextBox WtoTB;
+        private System.Windows.Forms.RichTextBox mensajeTB;
+        private System.Windows.Forms.RichTextBox WmensajeTB;
+        private System.Windows.Forms.Button smssendBT;
+        private System.Windows.Forms.Button WsmsBT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox detalleTB;
+        private System.Windows.Forms.RichTextBox WdetalleTB;
+    }
+}
+
---
+++ EjemploTwilio/EjemploTwilio/EnvioMensajes.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace EjemploTwilio
+{
+    public partial class EnvioMensajes : Form
+    {
+        public EnvioMensajes()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void smssendBT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //ID de la Cuenta del proyecto de Twilio Creado en la Pagina
+                var accountSid = "ACb03a93862115f88222ca8142d7988c94";
+
+                //Codigo de verificacion de la cuenta
+                var authToken = "1eab81685fabd3213cbead05074da6cb";
+
+                //inicializa el servicio del Twilio
+                TwilioClient.Init(accountSid, authToken);
+
+                //Creamos un nuevo Mensaje, se inicializa con el numero que recibe el mensaje
+                var messageOptions = new CreateMessageOptions(new PhoneNumber($"+506{toTB.Text}"));
+
+                //agregamos el numero que envia, este nos lo da Twilio
+                messageOptions.From = new PhoneNumber(senderTB.Text);
+
+                //Llenamos el cuerpo del mensaje
+                messageOptions.Body = mensajeTB.Text;
+                
+
+                // se envia el mensaje
+                var message = MessageResource.Create(messageOptions);
+
+
+
+
+                detalleTB.Text = $"De: {message.From}\n";
+                detalleTB.Text += $"Para: {message.To}\n";
+                detalleTB.Text += $"Mensaje: {message.Body}\n";
+            }
+            catch (Exception ex)
+            {
+                detalleTB.Text = ex.Message;
+                
+            }
+        }
+
+        private void WsmsBT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var accountSid = "ACb03a93862115f88222ca8142d7988c94";
+                var authToken = "1eab81685fabd3213cbead05074da6cb";
+                TwilioClient.Init(accountSid, authToken);
+
+                var messageOptions = new CreateMessageOptions(new PhoneNumber($"whatsapp:+506{WtoTB.Text}"));
+                messageOptions.From = new PhoneNumber($"whatsapp:{WsenderTB.Text}");
+                messageOptions.Body = WmensajeTB.Text;
+                
+
+                var message = MessageResource.Create(messageOptions);
+                Console.WriteLine(message.Body);
+                WdetalleTB.Text = $"De: {message.From}\n";
+                WdetalleTB.Text += $"Para: {message.To}\n";
+                WdetalleTB.Text += $"Mensaje: {message.Body}\n";
+            }
+            catch (Exception ex)
+            {
+
+                detalleTB.Text = ex.Message;
+            }
+        }
+    }
+}
